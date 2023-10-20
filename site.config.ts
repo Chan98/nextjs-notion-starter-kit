b848b4d7e7f083306936168ee143ae23b0b63d68
parkchanmi <pcm@cudo.co.kr>
2023-10-20 15:13:15 +0900

    config 변경

--- site.config.ts
+++ site.config.ts
@@ -9,17 +9,17 @@ export default siteConfig({
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Chaniii\'s Blog',
+  domain: 'blog.chaniii.dev',
+  author: 'Chaniii',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'Chaniii\'s Blog',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  // twitter: 'transitive_bs',
+  github: 'Chan98',
+  linkedin: '찬미-박-628052241/',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
